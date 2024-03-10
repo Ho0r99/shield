f142e8570bb736682d15bd4026e38edcd0055062
Hoor mohammed <157906970+Ho0r99@users.noreply.github.com>
2024-03-10 08:04:22 +0300

    add media and static files

--- projec/settings.py
+++ projec/settings.py
@@ -113,9 +113,16 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.0/howto/static-files/
+# https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
+MEDIA_URL='media/'
+MEDIA_ROOT=BASE_DIR / "media"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
